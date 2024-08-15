bc8b4c6a411af9530e4b5b80f390129755ebc2ea
enricofs <enricoferreiradossantos@gmail.com>
2024-08-15 19:38:48 -0300

    dark mode adjustment

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
-const toggleSwitch = document.querySelector(".theme-switch");
+const toggleSwitch = document.querySelector(
+  '.theme-switch input[type="checkbox"]'
+);
 const currentTheme = localStorage.getItem("theme");
 
 if (currentTheme) {
   document.documentElement.setAttribute("data-theme", currentTheme);
 
-  if (currentTheme == "dark") {
+  if (currentTheme === "dark") {
     toggleSwitch.checked = true;
   }
 }
